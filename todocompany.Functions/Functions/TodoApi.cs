48500410b7eecd595fdde5385f2a1b09ccd7c670
juandv30 <78824982+juandv30@users.noreply.github.com>
2021-09-05 00:56:03 -0500

    put update time in process

--- todocompany.Functions/Functions/TodoApi.cs
+++ todocompany.Functions/Functions/TodoApi.cs
@@ -19,7 +19,7 @@ namespace todocompany.Functions.Functions
         [FunctionName(nameof(ConsolidateProcess))]
         public static async Task<IActionResult> ConsolidateProcess(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "consolidated")] HttpRequest req,
-            [Table("consolidated", Connection = "AzureWebJobsStorage")] CloudTable consolidatedTable,
+            [Table("consolidated", Connection = "AzureWebJobsStorage")] CloudTable companyTable,
             ILogger log)
         {
             log.LogInformation("recieved process for consolidated");
@@ -48,7 +48,7 @@ namespace todocompany.Functions.Functions
             };
 
             TableOperation addOperation = TableOperation.Insert(todoEntity);
-            await consolidatedTable.ExecuteAsync(addOperation);
+            await companyTable.ExecuteAsync(addOperation);
 
             string message = "New proccess consolidate in the table";
             log.LogInformation(message);
@@ -66,7 +66,7 @@ namespace todocompany.Functions.Functions
         [FunctionName(nameof(CreateTime))]
         public static async Task<IActionResult> CreateTime(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "time")] HttpRequest req,
-            [Table("time", Connection = "AzureWebJobsStorage")] CloudTable timeTable,
+            [Table("time", Connection = "AzureWebJobsStorage")] CloudTable companyTable,
             ILogger log)
         {
             log.LogInformation("Time recieved");
@@ -94,9 +94,57 @@ namespace todocompany.Functions.Functions
             };
 
             TableOperation addOperation = TableOperation.Insert(todoEntity);
-            await timeTable.ExecuteAsync(addOperation);
+            await companyTable.ExecuteAsync(addOperation);
 
             string message = "New time consolidate in the table";
+            log.LogInformation(message);
+
+
+            return new OkObjectResult(new Response
+            {
+                IsSuccess = true,
+                Message = message,
+                Result = todoEntity
+            });
+        }
+
+
+        [FunctionName(nameof(UpdateTime))]
+        public static async Task<IActionResult> UpdateTime(
+                       [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "time/{id}")] HttpRequest req,
+                       [Table("time", Connection = "AzureWebJobsStorage")] CloudTable companyTable,
+                       string id,
+                       ILogger log)
+        {
+            log.LogInformation($"Update time employed: {id}, received");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            Entry entry = JsonConvert.DeserializeObject<Entry>(requestBody);
+
+            //validate employed id
+            TableOperation findOperation = TableOperation.Retrieve<TodoEntity>("ENTRY", id);
+            TableResult findResult = await companyTable.ExecuteAsync(findOperation);
+            if (findResult == null)
+            {
+                return new BadRequestObjectResult(new Response
+                {
+                    IsSuccess = false,
+                    Message = "Employed id not found."
+                });
+            }
+
+            //update employed time
+            TodoEntity todoEntity = (TodoEntity)findResult.Result;
+            todoEntity.IsConsolidated = entry.IsConsolidated;
+            if (!string.IsNullOrEmpty(entry.EmployedId))
+            {
+                todoEntity.EmployedId = entry.EmployedId;
+            }
+
+            TableOperation addOperation = TableOperation.Replace(todoEntity);
+            await companyTable.ExecuteAsync(addOperation);
+
+            string message = $"Entry: {id}, update in the table.";
             log.LogInformation(message);
 
 
