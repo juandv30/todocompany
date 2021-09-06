b405cf965be025b128cf872d8a0c2d6b0d00b2fe
juandv30 <78824982+juandv30@users.noreply.github.com>
2021-09-05 21:31:13 -0500

    scheduled functions

---
+++ todocompany.Functions/Functions/ScheduledFunction.cs
@@ -0,0 +1,33 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Threading.Tasks;
+using todocompany.Functions.Entities;
+
+namespace todocompany.Functions.Functions
+{
+    public static class ScheduledFunction
+    {
+        [FunctionName("ScheduledFunction")]
+        public static async Task Run(
+            [TimerTrigger("0 */1 * * * *")] TimerInfo myTimer,
+            [Table("time", Connection = "AzureWebJobsStorage")] CloudTable companyTable,
+
+            ILogger log)
+        {
+            log.LogInformation($"Deleting completed function executed at: {DateTime.Now}");
+            string filter = TableQuery.GenerateFilterConditionForBool("IsCompleted", QueryComparisons.Equal, true);
+            TableQuery<TodoEntity> query = new TableQuery<TodoEntity>().Where(filter);
+            TableQuerySegment<TodoEntity> completedEntrys = await companyTable.ExecuteQuerySegmentedAsync(query, null);
+            int deleted = 0;
+            foreach (TodoEntity completedEntry in completedEntrys)
+            {
+                await companyTable.ExecuteAsync(TableOperation.Delete(completedEntry));
+                deleted++;
+            }
+            log.LogInformation($"Deleted: {deleted} items at: {DateTime.Now}");
+
+        }
+    }
+}
